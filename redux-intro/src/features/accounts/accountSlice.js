66ed088d2685fc4905b2fad354f85fedc3b38f9c
quonly <amornthep.mos07@gmail.com>
2023-08-21 06:58:27 +0700

    setup fast pizza

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -1,11 +1,83 @@
-const initialStateAccount = {
+/*
+createSlice
+ 1. It'll automatically create action creators from our reducers
+ 2. It makes writing these reducers a lot easier because we no longer need that switch statement and the default case is automatically handled.
+ 3. We can actually mutate now the state directly inside of our reducers because createSlice uses Immer under the hood.  
+ */
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 }
 
-export default function accountReducer(state = initialStateAccount, action) {
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    // One reducer per action
+    deposit(state, action) {
+      state.balance += action.payload
+      state.isLoading = false
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        // return a new object whic will then become the payload object in the reducer.
+        return {
+          payload: { amount, purpose },
+        }
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return // We no longer need to return the entire state object
+
+        state.loan = action.payload.amount
+        state.balance = state.balance + action.payload.amount
+        state.loanPurpose = action.payload.purpose
+      },
+    },
+    payLoan(state, action) {
+      state.loanPurpose = ""
+      state.balance -= state.loan
+      state.loan = 0
+    },
+    convertingCurrency(state, action) {
+      state.isLoading = true
+    },
+  },
+}) // Accepts an object with 3 properties: name, initialState, reducers
+
+// console.log(accountSlice)
+// We need to take them out of that objects and export them separately
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions // Action creators
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount }
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" })
+    // API call
+    console.log(getState())
+    const host = "api.frankfurter.app"
+    const res = await fetch(
+      `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`
+    )
+    const data = await res.json()
+    const converted = data.rates.USD
+    // return action
+    dispatch({ type: "account/deposit", payload: converted })
+  }
+}
+
+export default accountSlice.reducer // Reducer function
+
+/*
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -68,3 +140,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: "account/payLoan" }
 }
+*/
