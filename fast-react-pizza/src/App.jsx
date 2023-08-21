81f1984d1c9de79dc428d63012c73d4352fb7f4c
quonly <amornthep.mos07@gmail.com>
2023-08-21 21:03:56 +0700

    render as you fetch

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,32 +1,43 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
 import Home from "./ui/Home"
-import Menu from "./features/menu/Menu"
+import Menu, { loader as menuLoader } from "./features/menu/Menu"
 import Cart from "./features/cart/Cart"
 import CreateOrder from "./features/order/CreateOrder"
 import CreateUser from "./features/user/CreateUser"
 import Order from "./features/order/Order"
+import AppLayout from "./ui/AppLayout"
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
+    element: <AppLayout />, // with out path we call its layout route
+    children: [
+      // accept another array of route
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        // 1 create loader
+        // 2 provide loader function
+        loader: menuLoader,
+        // 3 provide data to the page
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
   },
 ])
 
--- fast-react-pizza/src/features/menu/Menu.jsx
+++ fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
+import { useLoaderData } from "react-router-dom"
+import { getMenu } from "../../services/apiRestaurant"
+import MenuItem from "./MenuItem"
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData() // data comes from loader function. useEffect = fetch on render and loader = render as you fetch
+
+  return (
+    <ul>
+      {menu.map(pizza => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  )
 }
 
-export default Menu;
+export async function loader() {
+  // fetch the data and then return it
+  const menu = await getMenu()
+  return menu
+}
+
+export default Menu
