ff6e59e06f2b15201bae52417d2fdbfd42ac807a
quonly <amornthep.mos07@gmail.com>
2023-08-25 17:54:07 +0700

    create deleteCabin fn

--- 17-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import { formatCurrency } from "../../utils/helpers"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { deleteCabin } from "../../services/apiCabins"
 
 const TableRow = styled.div`
   display: grid;
@@ -10,7 +13,7 @@ const TableRow = styled.div`
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-grey-100);
   }
-`;
+`
 
 const Img = styled.img`
   display: block;
@@ -19,22 +22,51 @@ const Img = styled.img`
   object-fit: cover;
   object-position: center;
   transform: scale(1.5) translateX(-7px);
-`;
+`
 
 const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
   font-family: "Sono";
-`;
+`
 
 const Price = styled.div`
   font-family: "Sono";
   font-weight: 600;
-`;
+`
 
 const Discount = styled.div`
   font-family: "Sono";
   font-weight: 500;
   color: var(--color-green-700);
-`;
+`
+
+function CabinRow({ cabin }) {
+  const { id, name, maxCapacity, regularPrice, discount, image } = cabin
+
+  const queryClient = useQueryClient()
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: id => deleteCabin(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      })
+    },
+  })
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(id)} disabled={isDeleting}>
+        Delete
+      </button>
+    </TableRow>
+  )
+}
+
+export default CabinRow
--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,4 +1,9 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import { deleteCabin, getCabins } from "../../services/apiCabins"
+import { useMutation, useQuery } from "@tanstack/react-query"
+
+import Spinner from "../../ui/Spinner"
+import CabinRow from "./CabinRow"
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -7,7 +12,7 @@ const Table = styled.div`
   background-color: var(--color-grey-0);
   border-radius: 7px;
   overflow: hidden;
-`;
+`
 
 const TableHeader = styled.header`
   display: grid;
@@ -22,4 +27,35 @@ const TableHeader = styled.header`
   font-weight: 600;
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
-`;
+`
+
+function CabinTable() {
+  const { isLoading, data: cabins } = useQuery({
+    queryKey: ["cabins"], // unique key for this query used for caching and refetching and also for calling with useQueryClient
+    queryFn: getCabins, // should be return promise
+  })
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map(cabin => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  )
+}
+
+export default CabinTable
